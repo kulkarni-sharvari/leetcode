96a1a984959839a08c27f6ceb0b6b4c8832eda9d
sharvarikulkarni <kulkarni.sharvar@northeastern.edu>
2026-05-13 08:29:42 -0400

    binary search

---
+++ easy/704_BinarySearch.py
@@ -0,0 +1,35 @@
+# 704. Binary Search - https://leetcode.com/problems/binary-search/description/
+
+# Description
+# Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+# Example 1:
+# Input: nums = [-1,0,3,5,9,12], target = 9
+# Output: 4
+# Explanation: 9 exists in nums and its index is 4
+
+# Example 2:
+# Input: nums = [-1,0,3,5,9,12], target = 2
+# Output: -1
+# Explanation: 2 does not exist in nums so return -1
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        n = len(nums)
+        low = 0
+        high = n-1
+        while low<=high:
+            idx = low + (high - low )//2 
+            if target == nums[idx]:
+                return idx
+            elif target < nums[idx]:
+                high = idx-1
+            else:
+                low = idx+1
+        return -1
+
+# Time complexity: O(nlgn)
+# Space complexity: O(1)
